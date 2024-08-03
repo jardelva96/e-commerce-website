074e96ca5566684f95d0a89b1f91689737b1fcfd
JARDEL ALVES <jardel.va96@gmail.com>
2024-08-03 20:37:11 -0300

    Update store.js

--- client/client/client/src/redux/store.js
+++ client/client/client/src/redux/store.js
@@ -1,1 +1,16 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import productReducer from './reducers/productReducer';
+
+const rootReducer = combineReducers({
+  products: productReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
 
